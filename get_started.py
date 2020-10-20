c0010d3e33e36c82eae1a076b82367b677a571e7
beyarkay <boydrkane@gmail.com>
2020-10-20 20:27:47 +0200

    Removing the check-username script

--- get_started.py
+++ get_started.py
@@ -21,46 +21,33 @@ else:
     username = input("Your username: ")
 
 while True:
-    # Check that the username isn't already taken
-    if DEBUG: print(server_url + "/check_username.php")
-    r = requests.post(
-        server_url + "/check_username.php",
-        json={'username': username}
-    )
+    abbreviations = [str(c).lower() for c in username if c.isalpha()][0:1]
+    # Register the client with the server.
+    json = {
+        "username": username,
+        "abbreviations": abbreviations,
+        "url": client_url,
+    }
+    r = requests.post(server_url + "/register_client.php", json=json)
+    if DEBUG: print(r.text)
     return_data = r.json()
     if return_data['status'] == 'good':
-        break
-    print("That username has already been taken, please choose another.")
-    username = input("Choose a username: ")
+        # Update the config.json file
+        with open(CONFIG_FILE, 'r') as config_file:
+            config = json.load(config_file)
+        config['username'] = username
+        config['abbreviations'] = abbreviations
+        config['url'] = client_url
+        config['server_url'] = server_url
+        with open(CONFIG_FILE, 'w+') as config_file:
+            json.dump(config, config_file, indent=2)
+        
+        if DEBUG: print("Config file updated: " + str(config))
 
-abbreviations = [str(c).lower() for c in username if c.isalpha()][0:1]
-
-print("The server will use these letters: {} as "
-      "a 1-letter abbreviation in order to represent you."
-      .format(abbreviations))
-# TODO ask the user if they want to change this
-
-# Register the client with the server.
-json = {
-    "username": username,
-    "abbreviations": abbreviations,
-    "url": client_url,
-}
-if DEBUG: print("POST to {} with '{}'".format(server_url + "/register_client.php", json))
-r = requests.post(server_url + "/register_client.php", json=json)
-if DEBUG: print(r.text)
-return_data = r.json()
-if not return_data['status'] == 'good':
-    # Update the config.json file
-    with open(CONFIG_FILE, 'r') as config_file:
-        config = json.load(config_file)
-    config['username'] = username
-    config['abbreviations'] = abbreviations
-    config['url'] = client_url
-    config['server_url'] = server_url
-    with open(CONFIG_FILE, 'w+') as config_file:
-        json.dump(config, config_file, indent=2)
-
-    print("All Complete.\nGo to {} to see the server".format(server_url))
-else:
-    print("Error occured during client registration: {}".format(return_data))
+        print("All Complete.\nGo to {} to see the server".format(server_url))
+    elif return_data['cause'] == 'username':
+        print("That username has already been taken, please choose another.")
+        username = input("Choose a username: ")
+        continue
+    else:
+        print("Error occured during client registration: {}".format(return_data))
